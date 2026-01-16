05303ff1eb2a68ad55ae4e3675aa085aea22ea81
smalif <alif019950@gmail.com>
2026-01-16 22:49:15 +0600

    Sql Added

---
+++ SQLQuery2.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE LanQuizerDB;
+GO
+
+USE LanQuizerDB;
+GO
+
+CREATE TABLE Teachers (
+    TeacherID VARCHAR(12) PRIMARY KEY UNIQUE,      -- Example: 12-53700-3
+    teacherEmail VARCHAR(100) NOT NULL UNIQUE,
+    TeacherpassBox VARCHAR(100) NOT NULL,
+    teacherName VARCHAR(100) NOT NULL,
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+GO
+
+INSERT INTO Teachers (TeacherID, teacherEmail, TeacherpassBox, teacherName)
+VALUES
+('23-53700-3', 'admin@aiub.edu', 'admin', 'ADMIN'),
+('12-34567-8', 'teacher@aiub.com', 'admin', 'Kazi Asif Ahmed');
+GO
+
+Select * from Teachers;
+
+
